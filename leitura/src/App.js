faed86ca0c254a62060167e9c36c24db360d34e9
marcioc0st <marcio@vizir.com.br>
2019-02-10 23:10:50 -0200

    if post is undefined

--- leitura/src/App.js
+++ leitura/src/App.js
@@ -20,7 +20,11 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={AllPostsPage} />
           <Route path="/add-post" component={AddPostPage} />
-          <Route path="/post/:id" component={PostDetail} />
+          <Route path="/post/:id" render={props => {
+            const { location } = props;
+            if(location.state === undefined) return <Page404 />
+            return <PostDetail {...props} />
+          }} />
           <Route path="/edit-post/:id" component={EditPost} />
           <Route path="/:category" render={props => {
             const activeCategory = getCategoryName(props.location.pathname);
